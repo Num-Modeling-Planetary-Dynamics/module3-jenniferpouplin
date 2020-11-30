378977d3fcdb00f52bd376b5272a7eee32b5ce10
Jennifer <jpouplin@purdue.edu>
2020-11-30 12:28:53 -0500

    module3_problem2 start on jupyter notebook -

--- src/xv2el.f90
+++ src/xv2el.f90
@@ -3,12 +3,13 @@ program xv2el
    use, intrinsic :: iso_fortran_env ! Use the intrinsic kind definitions
    implicit none
    integer, parameter :: P = real64
-   real(P) :: hnorm, e, inc, long, varpi, PI, f, mu, om, a, argr, argv
-   integer :: i, t, iarg, w 
-   CHARACTER :: argu
-   real(P), dimension(3) :: h, r, v , ecc , n, vcrossh,z
+   real(P) :: e, inc, long, varpi, PI, f, mu, om, a, argr, argv
+   integer :: iarg, w
+   logical :: new 
+   CHARACTER (len = 4) :: argu
+   real(P), dimension(3) :: h, r, v , ecc , n, vcrossh, z
    PI = 2*asin(1.0_P)
-
+   new = .False. 
    iarg = iargc()
       do  w = 1, iarg
          call getarg( w, argu )
@@ -33,12 +34,14 @@ program xv2el
             call getarg( w+3, argu )
             Read(argu,*) argv 
             v(3)=argv
+         CASE("-new")
+            new = .True.
          END SELECT
       enddo
 
-   
+   WRITE(*,*) 'new = ', new 
    mu = 1._P 
-   z = [0, 0,  1]
+   z = [0.0_P, 0.0_P,  1.0_P]
    CALL cross(r, v, h)
    CALL cross(v, h, vcrossh)
    ecc = (1._P / mu)*vcrossh - r / norm2(r)
@@ -46,9 +49,11 @@ program xv2el
    IF (dot_product(r,v) >= 0 ) THEN 
       f = acos((dot_product(ecc,r)/(norm2(r)*e)))
    ELSE 
-      f = 2*PI - acos((dot_product(ecc,r)/(norm2(r)*e)))
+      f = 2.0_P*PI - acos((dot_product(ecc,r)/(norm2(r)*e)))
    END IF 
-
+   WRITE(*,*) "ecc = ", ecc
+   WRITE(*,*) "r = ", r 
+   WRITE(*,*) "v = ", v
    inc = acos(h(3)/norm2(h))
 
    e = norm2(ecc)
@@ -61,18 +66,21 @@ program xv2el
    IF (n(2) >=0) THEN 
       om = acos(dot_product(n,ecc)/norm2(n)/e)
    ELSE 
-      om = 2*PI - acos(dot_product(n,ecc)/norm2(n)/e)
+      om = 2.0_P*PI - acos(dot_product(n,ecc)/norm2(n)/e)
    END IF 
 
    varpi = long + om 
 
-   a = 1._P/((2/norm2(r))- (norm2(v)**2)/mu)
+   a = 1._P/((2.0_P/norm2(r))- (norm2(v)**2.0_P)/mu)
 
+   if (new) THEN
+      open(15,file='xv2el.output', action = 'write')
+   else 
+      open(15,file='xv2el.output', status= 'old', action = 'write', position = 'append')
+   end if 
 
-   open(15,file='xv2el.output')
-
-   write(15,*) a,e,inc,long,varpi,f
-
+   write(15,*) a,',',e,',',inc,',',long,',',varpi,',',f
+   close(15)
 
 end program xv2el
 
